bfd36772fcf1365c16afd4865c71e280a1a36ce6
leisson <leissonalvarez@gmail.com>
2025-05-25 14:13:35 -0500

    modificando nary components

--- NariTrade/src/app/services/nary-component.service.ts
+++ NariTrade/src/app/services/nary-component.service.ts
@@ -1,12 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
+  
+
 })
 export class NaryComponentService {
 
   constructor(private http: HttpClient) { }
+  apiUri = '/api/micomponente';
+  httpOptons = new HttpHeaders().set('Content-Type', 'application/json');
 }
