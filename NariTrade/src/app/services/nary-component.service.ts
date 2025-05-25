8a8a4e61f5c8c6cf2214835d242170094af681d8
leisson <leissonalvarez@gmail.com>
2025-05-25 15:07:27 -0500

    llamado a nuevos componentes

--- NariTrade/src/app/services/nary-component.service.ts
+++ NariTrade/src/app/services/nary-component.service.ts
@@ -2,15 +2,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
-  
-
+  providedIn: 'root',
 })
 export class NaryComponentService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   apiUri = '/api/micomponente';
   httpOptons = new HttpHeaders().set('Content-Type', 'application/json');
+
+  getAllmicomponenteData(): Observable<any> {
+    return this.http.get<any>(this.apiUri);
+  }
+  newmicomponente(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUri, data, { headers: this.httpOptons });
+  }
 }
