46d7650e534690e6a2882e3e44ba07f06fa996b6
leisson <leissonalvarez@gmail.com>
2025-05-25 18:06:54 -0500

    depurando codigo

--- NariTrade/src/app/app.config.ts
+++ NariTrade/src/app/app.config.ts
@@ -1,18 +1,28 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-  provideRouter(routes), provideClientHydration(withEventReplay()),
-  provideHttpClient(),
-  importProvidersFrom(BrowserModule, ToastrModule.forRoot()),
-  provideToastr(),
-  provideAnimations(),
-]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule, ToastrModule.forRoot()),
+    provideToastr(),
+    provideAnimations(),
+  ],
 };
