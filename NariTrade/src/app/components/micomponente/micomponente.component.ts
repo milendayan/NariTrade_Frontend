eec0c125ffc4d5f26772739bfd591e70335a3737
leisson <leissonalvarez@gmail.com>
2025-05-25 15:25:44 -0500

    modificando errores

--- NariTrade/src/app/components/micomponente/micomponente.component.ts
+++ NariTrade/src/app/components/micomponente/micomponente.component.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-micomponente',
   imports: [],
   templateUrl: './micomponente.component.html',
-  styleUrl: './micomponente.component.css'
+  styleUrl: './micomponente.component.css',
 })
 export class MicomponenteComponent {
+  micomponeteList: any = [];
 
+  constructor(private micomponenteService: MicomponenteComponent) {}
+  getAllAnimals() {
+    this.micomponenteService.getAllmicomponeteData().subscribe((data: {}) => {
+      this.micomponeteList = data;
+    });
+  }
+  getAllmicomponeteData() {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit() {
+    this.getAllmicomponete();
+  }
+  getAllmicomponete() {
+    throw new Error('Method not implemented.');
+  }
 }
