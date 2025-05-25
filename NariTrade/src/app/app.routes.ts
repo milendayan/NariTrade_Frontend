533534ee6b6318dfeda449a0cf98065138f886a4
leisson <leissonalvarez@gmail.com>
2025-05-25 16:17:33 -0500

    correccion de errores

--- NariTrade/src/app/app.routes.ts
+++ NariTrade/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NaryComponentsComponent } from './components/nary-components/nary-components.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: NaryComponentsComponent },
+];
--- NariTrade/src/app/components/nary-components/nary-components.component.ts
+++ NariTrade/src/app/components/nary-components/nary-components.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NaryComponentService } from '../../services/nary-component.service';
 
 @Component({
   selector: 'app-nary-components',
   imports: [],
   templateUrl: './nary-components.component.html',
-  styleUrl: './nary-components.component.css'
+  styleUrl: './nary-components.component.css',
 })
 export class NaryComponentsComponent {
+  naryList: any = [];
 
+  constructor(private naryService: NaryComponentService) {}
+  getAllAnimals() {
+    this.naryService.getAllnaryData().subscribe((data: {}) => {
+      this.naryList = data;
+    });
+  }
+  ngOnInit() {
+    this.getAllAnimals();
+  }
 }
