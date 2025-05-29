03d53ae9f60a26f93c74cbe90b11def21ea7ff0c
leisson <leissonalvarez@gmail.com>
2025-05-29 18:47:53 -0500

    Estadisiticas

--- NariTrade/src/app/estadisticas/estadisticas.component.ts
+++ NariTrade/src/app/estadisticas/estadisticas.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../services/auth.services';
+import { CommonModule } from '@angular/common';
+import { Router,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [],
+   imports: [CommonModule, RouterModule],
   templateUrl: './estadisticas.component.html',
-  styleUrl: './estadisticas.component.css'
+  styleUrls: ['./estadisticas.component.css']
 })
-export class EstadisticasComponent {
+export class EstadisticasComponent implements OnInit {
+  estadisticas: any;
+  isLoading = true;
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    public authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.obtenerEstadisticas();
+  }
+
+  obtenerEstadisticas(): void {
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+
+    this.http.get<any>('http://localhost:3070/NariTrade/User/estadisticas', { headers })
+      .subscribe({
+        next: (data) => {
+          this.estadisticas = data;
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.error('Error al cargar estadísticas:', error);
+          this.isLoading = false;
+        }
+      });
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: (err) => console.error('Error al cerrar sesión:', err)
+    });
+  }
 }
